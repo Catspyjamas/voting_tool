9dfaebc40f5f3315044ad231ba787bc27a0f6421
Catspyjamas <judith.urban@peerigon.com>
2019-03-18 13:28:01 +0100

    removed old skills file

--- src/router.js
+++ src/router.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Skills from './components/Skills.vue'
 import About from './components/About.vue'
 import VotingDetails from './components/VotingDetails.vue'
 
@@ -9,8 +8,8 @@ Vue.use(Router)
 export default new Router({
   routes: [{
       path: '/',
-      name: 'skills',
-      component: Skills
+      name: 'about',
+      component: About
     },
     {
       path: '/about/:name',
